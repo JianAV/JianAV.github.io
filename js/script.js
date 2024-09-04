385da3760d189fc019e0fba0e37097d0f2bf21b8
Jian Verdad <jian.verdad@gmail.com>
2024-09-04 12:55:41 -0700

    reformat local repo, test push

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-// JavaScript source code
+
+window.onscroll = function(){
+    var currentScrollPos = window.pageYOffset || window.scrollY;
+    if(currentScrollPos == 0){
+        document.getElementById("navbar").style.backgroundColor = "inherit";
+    }
+    else{
+        document.getElementById("navbar").style.backgroundColor = "rgb(255,255,255)";
+    }
+    document.getElementById("Ypos").innerHTML = window.scrollY;
+}
