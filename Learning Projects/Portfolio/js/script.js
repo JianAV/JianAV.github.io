8284f98df32c9422b919ae402a0d06dbe1780368
Jian Verdad <jian.verdad@gmail.com>
2024-09-04 12:41:25 -0700

    test new page in GitHub Pages

--- Learning Projects/Portfolio/js/script.js
+++ Learning Projects/Portfolio/js/script.js
@@ -2,7 +2,7 @@
 window.onscroll = function(){
     var currentScrollPos = window.pageYOffset || window.scrollY;
     if(currentScrollPos == 0){
-        document.getElementById("navbar").style.backgroundColor = "transparent";
+        document.getElementById("navbar").style.backgroundColor = "inherit";
     }
     else{
         document.getElementById("navbar").style.backgroundColor = "rgb(255,255,255)";
